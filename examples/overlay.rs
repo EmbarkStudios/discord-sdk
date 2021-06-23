95d3aed16869cd49690ccd2dc80e04105e2f664e
Jake Shadle <jake.shadle@embark-studios.com>
2021-06-23 18:14:56 +0200

    Begin adding overlay example

---
+++ examples/overlay.rs
@@ -0,0 +1,98 @@
+#[path = "shared._rs"]
+mod shared;
+
+use discord_sdk as ds;
+use ds::overlay;
+use eframe::egui;
+
+struct App {
+    discord: ds::Discord,
+    handle: tokio::runtime::Handle,
+    overlay_state: tokio::sync::watch::Receiver<ds::wheel::OverlayState>,
+    signal: std::sync::Arc<(parking_lot::Mutex<bool>, parking_lot::Condvar)>,
+}
+
+impl eframe::epi::App for App {
+    fn name(&self) -> &str {
+        "discord-overlay"
+    }
+
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
+        macro_rules! run_async {
+            ($code:block) => {
+                self.handle.clone().block_on(async { $code })
+            };
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.group(|ui| {
+                let mut overlay_visible = {
+                    let os = self.overlay_state.borrow();
+
+                    ui.set_enabled(os.enabled);
+
+                    os.visible == overlay::Visibility::Visible
+                };
+
+                if ui.checkbox(&mut overlay_visible, "Show Overlay").clicked() {
+                    run_async!({
+                        match self
+                            .discord
+                            .set_overlay_visibility(if overlay_visible {
+                                overlay::Visibility::Visible
+                            } else {
+                                overlay::Visibility::Hidden
+                            })
+                            .await
+                        {
+                            Ok(_) => {
+                                let _ = self.overlay_state.changed().await;
+                            }
+                            Err(e) => {
+                                tracing::error!(error = ?e, "failed to change overlay visibility");
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    fn on_exit(&mut self) {
+        self.signal.1.notify_one();
+    }
+}
+
+fn main() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .thread_keep_alive(std::time::Duration::from_secs(1_000_000))
+        .thread_name("discord")
+        .enable_all()
+        .build()
+        .expect("failed to build tokio runtime");
+
+    let client = rt.block_on(async { shared::make_client(ds::Subscriptions::OVERLAY).await });
+
+    let handle = rt.handle().clone();
+
+    let rt_thread =
+        std::sync::Arc::new((parking_lot::Mutex::new(false), parking_lot::Condvar::new()));
+    let signal = rt_thread.clone();
+
+    let _handle = std::thread::spawn(move || {
+        let _guard = rt.enter();
+
+        rt_thread.1.wait(&mut rt_thread.0.lock());
+    });
+
+    let overlay_state = client.wheel.overlay().0;
+
+    let app = App {
+        discord: client.discord,
+        overlay_state,
+        signal,
+        handle,
+    };
+
+    eframe::run_native(Box::new(app), eframe::NativeOptions::default());
+}
